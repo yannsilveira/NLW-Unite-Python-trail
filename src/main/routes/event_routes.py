cbc128956d49313228aac49824cf83b86f713a93
Yann Ap. da Silveira <56853103+yannsilveira@users.noreply.github.com>
2024-04-02 23:32:55 -0300

    Crated the HTTP resquest and response pattern. Created the event handler class to handle the requests to register a new event.

---
+++ src/main/routes/event_routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, jsonify, request
+from src.http_types.http_request import HttpRequest
+from src.data.event_handler import EventHandler
+
+event_route_bp = Blueprint("event_route", __name__)
+
+@event_route_bp.route("/events", methods=["POST"])
+def create_event():
+    http_request = HttpRequest(body=request.json)
+    event_hanlder = EventHandler()
+
+    http_response = event_hanlder.register(http_request)
+    return jsonify(http_response.body), http_response.status_code 
